006956a4ed69f63c54fff011b5c305b2eb50b9ca
Katja Mähleke <katjamaehleke98@gmail.com>
2024-04-23 10:03:32 +0200

    Add AnimationComponents

---
+++ src/Components/StarsAnimation.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Lottie from "lottie-react";
+import starsAnimation from "../../public/Animations/Stars.json";
+
+const interactivity = {
+  mode: "scroll",
+  actions: [
+    {
+      visibility: [0],
+      type: "stop",
+      frames: [0],
+    },
+    {
+      visibility: [0.2, 0.45],
+      type: "seek",
+      frames: [0, 45],
+    },
+    {
+      visibility: [0.45, 1.0],
+      type: "loop",
+      frames: [45, 300],
+    },
+  ],
+};
+
+const StarsAnimation = () => {
+  return (
+    <Lottie
+      animationData={starsAnimation}
+      interactivity={interactivity}
+    />
+  );
+};
+
+export default StarsAnimation;
