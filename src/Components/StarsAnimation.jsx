b32516bd6c80b073e493b541329890e0ca7f575d
Katja Mähleke <katjamaehleke98@gmail.com>
2024-04-23 22:05:41 +0200

    add ="react" to client

--- src/Components/StarsAnimation.jsx
+++ src/Components/StarsAnimation.jsx
@@ -6,12 +6,17 @@ const interactivity = {
   mode: "scroll",
   actions: [
     {
-      visibility: [0, 0.2],
+      visibility: [0],
       type: "stop",
-      frames: [0, 45],
+      frames: [0],
     },
     {
       visibility: [0.2, 0.45],
+      type: "seek",
+      frames: [0, 45],
+    },
+    {
+      visibility: [0.45, 1.0],
       type: "seek",
       frames: [45, 300],
     },
