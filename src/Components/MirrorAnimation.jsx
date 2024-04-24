39b1682264e8fc147de348abedead35267bb6369
julievictoria98 <jura0002@stud.kea.dk>
2024-04-24 10:09:33 +0200

    v1

--- src/Components/MirrorAnimation.jsx
+++ src/Components/MirrorAnimation.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Lottie from "lottie-react";
 import mirrorAnimation from "../../public/Animations/Mirror.json";
@@ -25,10 +26,7 @@ const interactivity = {
 
 const MirrorAnimation = () => {
   return (
-    <Lottie
-      animationData={mirrorAnimation}
-      interactivity={interactivity}
-    />
+    <Lottie animationData={mirrorAnimation} interactivity={interactivity} />
   );
 };
 
